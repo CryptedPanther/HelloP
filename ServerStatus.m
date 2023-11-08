a2cb51ae7498cf4e3fdaead4af9f897d11f003a8
CryptedPanther <129084551+CryptedPanther@users.noreply.github.com>
2023-11-08 12:52:13 +0530

    Update ServerStatus.m

--- ServerStatus.m
+++ ServerStatus.m
@@ -1,4 +1,9 @@
 #^server_id = 896-482-90 {
-status_holder.HelloP('true.found')
-R&{HelloP.deployment()}
+status_holder.HelloP("true.found")
+R&{HelloP.deployment(pages-build-deployment{dynamic.workflow})}
+}
+#^server_id = 476-987-81 {
+status_holder.HelloP("false.Mfound")
+R&{HelloP.changes.m(pages-build-deployment{dynamic.workflow})}
+B&{HelloP.workflow.run(m.code{.workflow})}
 }
